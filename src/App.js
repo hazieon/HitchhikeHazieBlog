2db088a52971d398bd0763ac66b6a1cb04731101
Hazieon <handrew3721@gmail.com>
2021-04-22 02:02:30 +0100

    SET UP app with hero image in homepage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import Home from "./Components/Home"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <Router>
+      <div>
+        <nav className="nav-bar">
+          <ul>
+            <li className="home-link">
+             <Link to="/"> 
+              <div className="logo-box">
+               <img className="logo" src={"./logo.svg"} alt="logo"/>
+              </div></Link>
+            </li>
+            <li className="pages" id="cart">
+              <Link to="/checkout">🛒</Link>
+            </li>
+          </ul>
+        </nav>
+
+       
+        <Switch>
+          <Route path="/about">
+
+          </Route>
+          <Route path="/">
+          <Home/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
---
+++ src/Components/Home/index.js
@@ -0,0 +1,26 @@
+import styles from "./index.module.css"
+import {
+  Link
+} from "react-router-dom";
+
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+        <div className={styles.heroImage}>
+            <div className={styles.heroText}>
+               <h1>Hitchhike Hazie</h1>
+               <h2>Taking the scenic route.</h2>
+               <button className={styles.heroButton}>  
+                 <Link className={styles.link} 
+                     to="/sell">About Hazie</Link>
+              </button>
+            </div>
+        </div> 
+        </div>
+  );
+}
+
+export default Homepage;
+
+
