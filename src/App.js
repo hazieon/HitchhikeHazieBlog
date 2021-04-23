b7dcdea5741bda377bff3bfc574ca3e24f4fd5ab
Hazieon <handrew3721@gmail.com>
2021-04-23 01:46:53 +0100

    SECTION - images of skills, interests, etc added

--- src/App.js
+++ src/App.js
@@ -1,45 +1,40 @@
-import './App.css';
-import React, {useState} from "react";
-import Home from "./Components/Home"
-import CustomSlider from "./Components/Slider"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import "./App.css";
+import React, { useState } from "react";
+import Home from "./Components/Home";
+import CustomSlider from "./Components/Slider";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import logo from "./images/earth-hz.png";
 
 function App() {
   return (
     <div className="App">
- <Router>
-      <div>
-        <nav className="nav-bar">
-          <ul>
-            <li className="home-link">
-             <Link to="/"> 
-              <div className="logo-box">
-               <img className="logo" src={"./logo.svg"} alt="logo"/>
-              </div></Link>
-            </li>
-            <li className="pages" id="cart">
-              <Link to="/checkout">🛒</Link>
-            </li>
-          </ul>
-        </nav>
+      <Router>
+        <div>
+          <nav className="nav-bar">
+            <ul>
+              <li className="home-link">
+                <Link to="/">
+                  <div className="logo-box">
+                    <img className="logo" src={logo} alt="logo" />
+                  </div>
+                </Link>
+              </li>
+              <li className="pages" id="cart">
+                <Link to="/checkout">🛒</Link>
+              </li>
+            </ul>
+          </nav>
 
-       
-        <Switch>
-          <Route path="/about">
-
-          </Route>
-          <Route path="/">
-          <Home/>
-          <CustomSlider/>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+          <Switch>
+            <Route path="/about"></Route>
+            <Route path="/">
+              <Home />
+              <CustomSlider />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,26 +1,57 @@
-import styles from "./index.module.css"
-import {
-  Link
-} from "react-router-dom";
+import styles from "./index.module.css";
+import { Link } from "react-router-dom";
 
+import travelImg from "../../images/boseong.jfif";
+import skillsImg from "../../images/moongirl.jpg";
+import lifestyleImg from "../../images/received_2396607630564628.jpeg";
 
 function Homepage() {
   return (
     <div className="Homepage">
-        <div className={styles.heroImage}>
-            <div className={styles.heroText}>
-               <h1>Hitchhike Hazie</h1>
-               <h2>Take the scenic route.</h2>
-               <button className={styles.heroButton}>  
-                 <Link className={styles.link} 
-                     to="/sell">About Hazie</Link>
-              </button>
-            </div>
-        </div> 
+      <div className={styles.heroImage}>
+        <div className={styles.heroText}>
+          <h1>Hitchhike Hazie</h1>
+          <h2>Take the scenic route.</h2>
+          <button className={styles.heroButton}>
+            <Link className={styles.link} to="/sell">
+              About Hazie
+            </Link>
+          </button>
         </div>
+      </div>
+
+      <h2 className={styles.explainTitle}>My Interests</h2>
+      <div className={styles.explainBox}>
+        <div>
+          <h4>Travel & Adventure</h4>
+          <img
+            className={styles.explainImage}
+            id={styles.explainImageOne}
+            src={travelImg}
+            alt="hitchhiking with backpack"
+          ></img>
+        </div>
+        <div>
+          <h4>Skills & Creativity</h4>
+          <img
+            className={styles.explainImage}
+            id={styles.explainImageTwo}
+            src={skillsImg}
+            alt="self published book"
+          ></img>
+        </div>
+        <div>
+          <h4>Lifestyle</h4>
+          <img
+            className={styles.explainImage}
+            id={styles.explainImageThree}
+            src={lifestyleImg}
+            alt="freedom life"
+          ></img>
+        </div>
+      </div>
+    </div>
   );
 }
 
 export default Homepage;
-
-
