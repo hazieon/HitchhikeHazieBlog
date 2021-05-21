cd63d65c6a2120527ee8ee3718841e609b49e0ff
Hazieon <handrew3721@gmail.com>
2021-05-21 23:30:57 +0100

    Blog page progress - Styles structure and content in place

--- src/Components/blogPosts.js
+++ src/Components/blogPosts.js
@@ -28,6 +28,33 @@ const blogPosts = [
     },
     date: "20 May 2020",
   },
+  {
+    id: 2,
+    title: "test 2 ",
+    subtitle: "test 2 testtesttest test 2",
+    thumbnail:
+      "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg",
+    topics: ["d", "e", "fff"],
+    summary:
+      "testtest testtesttest test testtest test testtest test test testtest test testtest testtest test",
+    content: {
+      textOne:
+        "test testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestv",
+      textTwo:
+        "test testtesttesttest shsgesoingsiongigotest testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestvegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestv",
+      textThree:
+        "test testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg test testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestvtest testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestvtest testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestvtest testtesttesttest shegois seg gs g s gsdgsdg se g sdg sg dg sdg s gsg dg sdgssngiosneoisn sg ksdgjsnoig deg sgisgiotest testtestv test  testtesttestvtestvsgisgiotest testtestv test  testtesttestvtestv",
+    },
+    images: {
+      imageOne:
+        "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg",
+      imageTwo:
+        "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg",
+      imageThree:
+        "https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg",
+    },
+    date: "20 May 2020",
+  },
   //   {
   //     id: 2,
   //     title: "",
